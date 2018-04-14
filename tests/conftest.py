540f046468b1b5e8163e2abdb16e5fdca94733e2
Aneta <anetakanka@gmail.com>
2018-04-14 21:07:07 +0200

    Delete not used functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,12 +23,7 @@ def oneTimeSetUp(request, browser):
 
 def pytest_addoption(parser):
     parser.addoption("--browser")
-    parser.addoption("--osType", help="Type of operating system")
 
 @pytest.fixture(scope="session")
 def browser(request):
     return request.config.getoption("--browser")
-
-@pytest.fixture(scope="session")
-def osType(request):
-    return request.config.getoption("--osType")
