51864bc459eeeedc0c4444c5adf820e120524cf3
Aneta <anetakanka@gmail.com>
2018-04-14 21:02:53 +0200

    Delete not used functions

--- base/basepage.py
+++ base/basepage.py
@@ -7,12 +7,9 @@ It implements methods which are common to all the pages throughout the applicati
 This class needs to be inherited by all the page classes
 This should not be used by creating object instances
 
-Example:
-    Class LoginPage(BasePage)
 """
 from base.selenium_driver import SeleniumDriver
 from traceback import print_stack
-from utilities.util import Util
 
 
 class BasePage(SeleniumDriver):
@@ -26,19 +23,3 @@ class BasePage(SeleniumDriver):
         """
         super(BasePage, self).__init__(driver)
         self.driver = driver
-        self.util = Util()
-
-    def verifyPageTitle(self, titleToVerify):
-        """
-        Verify the page Title
-
-        Parameters:
-            titleToVerify: Title on the page that needs to be verified
-        """
-        try:
-            actualTitle = self.getTitle()
-            return self.util.verifyTextContains(actualTitle, titleToVerify)
-        except:
-            self.log.error("Failed to get page title")
-            print_stack()
-            return False
